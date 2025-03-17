ad4f22d1ffaf63b1579033bc2226422a160e92b8
umair <diaanantnag@gmail.com>
2025-03-17 15:35:14 +0530

    done

--- backend/spring-boot-ecom/src/main/java/com/example/spring/boot/ecom/HelloController.java
+++ backend/spring-boot-ecom/src/main/java/com/example/spring/boot/ecom/HelloController.java
@@ -1,9 +1,6 @@
 package com.example.spring.boot.ecom;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -13,8 +10,8 @@ public class HelloController {
     public String hello() {
         return "Hello World";
     }
-    @PostMapping
-    public String post(@RequestParam String name) {
-        return "Hello"+name;
+    @PostMapping("/post")
+    public String post(@RequestBody String name) {
+        return "Hello "+name;
     }
 }
